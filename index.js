6604216cfaca2fa7dfa11cf18ac190f5937061d2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-05-12 23:17:23 -0400

    Minor message edit

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT ||
   console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
   process.exit(1);
 } else {
-  console.log('Good job, you have the variables!')
+  console.log('All required variables found!')
 }
