145f43a7b512de5073b823753f3ef1577b631614
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-05-12 23:16:59 -0400

    Added environment conditional to index

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 require('dotenv').config();
 
 const botkit = require('botkit');
+
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+  console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+  process.exit(1);
+} else {
+  console.log('Good job, you have the variables!')
+}
