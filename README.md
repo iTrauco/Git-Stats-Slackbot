4e3748ebaac2fba50589de5eebfea723ca4c7f04
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-05-15 18:51:35 -0400

    Configured bot and web server

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 1. Create GH repository
 2. Clone project to local machine
+* Run `npm init` to set up package.json file
+* Install `botkit` and `dotenv` dependencies
+* Run `touch .env .gitignore `
+* Add .env (app data security) and other defaults to `.gitignore`
+* Set up environment variables found in this repository, leave values empty
+* Run `touch index.js` 
 3. Create a Slack App (https://api.slack.com/apps/new)
 4. Navigate to 'Basic Information'
 * Add app credentials to their respective .env variables 
@@ -10,7 +16,7 @@
 3c. Create a bot user in app settings, set to always be on
 3d. Request to 'Install App to Workspace' (Usually denied for policy)
 3e. If denied, create test workspace then install app to workspace
-3. On your .env file set BOT_TOKEN to = Bot User OAuth Access Token
+3. On your .env file set `BOT_TOKEN = [Bot User OAuth Access Token]`
 5. Enable the Events API in app settings
 5a. Configure the Request URL
 5b. 
--- index.js
+++ index.js
@@ -8,3 +8,40 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT ||
 } else {
   console.log('All required variables found!')
 }
+
+// Configuring the bot
+var controller = botkit.slackbot({
+  json_file_store: './db_slackbutton_slash_command/', // Saves Slack conversations
+  debug: true, // Detailed console debugging
+  clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+ })
+
+// Connecting Node app to Slack app
+controller.configureSlackApp({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+  scopes: ['commands', 'bot'],
+})
+
+// Creating the BOT! 
+let bot = controller.spawn({
+  token: process.env.BOT_TOKEN,
+  incoming_webhook: {
+    url: 'WE_WILL_GET_TO_THIS'
+  }
+}).startRTM();
+
+// Controller includes a built-in web server function (TL;DR no Express)
+controller.setupWebserver(process.env.PORT, function(err, webserver){
+  controller.createWebhookEndpoints(controller.webserver);
+  controller.createOauthEndpoints(controller.webserver, 
+    function(err, req, res) {
+      if (err) {
+        res.status(500).send(`ERROR: $(err)`);
+      } else {
+        res.send('Success!');
+      }
+    });
+ });
+
