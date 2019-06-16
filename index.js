93ade5460050a2e533f25a589bd90d1435afcbe2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-06-16 19:40:28 -0400

    Bot working locally and configured to AW workspace

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const SlackBot = require('slackbots');
-const axios = require('axios');
+const SlackBot = require("slackbots");
+const axios = require("axios");
 
 // Bot setup, needs Bot User OAuth Access Token from Slack Permissions
 const bot = new SlackBot({
-  token: 'xoxb-630563835409-639092118726-Xls9RkCStk7SPfiTc8Y1uiDG',
-  name: 'Git Stats'
-})
+  token: "xoxb-630563835409-639092118726-Xls9RkCStk7SPfiTc8Y1uiDG",
+  name: "Git Stats"
+});
+
+// What happens when we start the bot?
+// bot.on("start", () => {
+//   const params = {
+//     icon_emoji: ":smile:"
+//   };
+
+//   bot.postMessageToChannel("git_stats_slackbot", "Hello creators!", params);
+// });
+
+// Error Handler
+bot.on('error', (err) => console.log(err));
+
+// Message Handler
+bot.on('message', (data) => {
+  if (data.type !== 'message') {
+    return;
+  }
+  console.log(data);
+});
